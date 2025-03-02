1a924ffd2ba745f28853cb0487d1b9b590b83cfd
maratdev0210 <maratforwork1@gmail.com>
2025-03-02 16:10:54 +0300

    new design for event table

--- src/features/bet/ui/HistoryVotesTable.tsx
+++ src/features/bet/ui/HistoryVotesTable.tsx
@@ -5,40 +5,39 @@ interface IHistoryVotesTable {
   historicalData: HistoricalDataPoint[];
 }
 
+const tableHeaders = ["Дата", "Yes", "No"];
+
 const HistoryVotesTable: React.FC<IHistoryVotesTable> = ({
   historicalData,
 }) => (
-  <div className="p-6 bg-white/5 dark:bg-white rounded-xl shadow-lg">
-    <h3 className="text-xl font-semibold mb-4 dark:text-black text-white/80">
+  <div className="p-6  bg-white/5 dark:bg-white rounded-xl shadow-lg">
+    <h3 className="text-xl text-center  font-semibold mb-8 dark:text-black text-white/80">
       История торгов
     </h3>
-    <div className="overflow-x-auto">
+    <div className="overflow-auto w-full rounded-md lg:w-3/4 mx-auto dark:bg-white bg-black border-gray-800/50 dark:border-gray-300 border-1">
       <table className="w-full">
-        <thead className="bg-gray-50/80">
+        <thead className="[&_tr]:border-b [&_tr]:border-gray-800/50 dark:[&_tr]:border-gray-300 hover:bg-gray-900/40 dark:hover:bg-gray-200/50">
           <tr>
-            <th className="text-left py-3 px-4">Дата</th>
-            <th className="text-center py-3 px-4 text-green-600">ДА</th>
-            <th className="text-center py-3 px-4 text-red-600">НЕТ</th>
+            {tableHeaders.map((header) => {
+              return (
+                <th
+                  className={`dark:text-gray-400 text-gray-400/50  h-10 px-2 text-left font-medium group-hover:cursor-pointer`}
+                >
+                  {header}
+                </th>
+              );
+            })}
           </tr>
         </thead>
         <tbody>
           {historicalData.map((point, i) => (
             <tr
               key={i}
-              className={
-                i % 2 === 0 ? "dark:bg-gray-50 bg-gray-50/70" : "bg-gray-50/70"
-              }
+              className="border-b dark:text-black text-white/75 border-gray-800/50 dark:border-gray-300 *:p-2 hover:bg-gray-900/40 dark:hover:bg-gray-100"
             >
-              <td className="py-3 px-4 dark:text-black text-white">
-                {new Date(point.date).toLocaleDateString("ru-RU")}
-              </td>
-              <td className="text-center py-3 px-4 font-medium">
-                {formatPrice(point.volumeYes)}
-              </td>
-
-              <td className="text-center py-3 px-4 font-medium">
-                {formatPrice(point.volumeNo)}
-              </td>
+              <td>{new Date(point.date).toLocaleDateString("ru-RU")}</td>
+              <td>{formatPrice(point.volumeYes)}</td>
+              <td>{formatPrice(point.volumeNo)}</td>
             </tr>
           ))}
         </tbody>
